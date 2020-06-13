9e33b2e9edf8ae184d810bc6bdf433d5a7f248f0
Oscar Perpiñán Lamigueiro <oscar.perpinan@upm.es>
2020-06-13 10:21:42 +0200

    Actualiza informacion de potencia instalada

--- figs/PVWorld.R
+++ figs/PVWorld.R
@@ -4,15 +4,14 @@ library(directlabels)
 library(RColorBrewer)
 library(latticeExtra)
 
-pvCum <- read.zoo('PVWorld.csv', sep = ';', header = TRUE, dec = ',')
-pv <- diff(pvCum)
+pvCum <- read.zoo('PVWorld.csv', sep = ';', header = TRUE)
 
 pdf('../figs/PVWorld.pdf')
-p <- xyplot(pv, superpose = TRUE, type = 'b',
-            ylab = 'Potencia FV (MW)',
+p <- xyplot(pvCum/1000, superpose = TRUE, type = 'b',
+            ylab = 'Potencia FV (GW)',
             par.settings = custom.theme.2(cex = 0.3,
                                           lwd = 2.5,
                                           pch = 21))
-direct.label(p, "top.points")
+direct.label(p)#, "top.points")
 dev.off()
 
