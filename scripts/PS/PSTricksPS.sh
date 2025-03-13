4f073f4db9fbd640d340eddcc3191a962904eeaa
Oscar Perpiñán Lamigueiro <oscar.perpinan@upm.es>
2025-03-13 10:12:57 +0100

    Bye

--- scripts/PS/PSTricksPS.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#$1 es el nombre del fichero TEX, pero sin .tex!!!
-cp $1.tex Fich.tex
-latex PSTricksPS.tex
-dvips PSTricksPS.dvi -E -o ../Figuras/$1.ps
-ps2pdf -dEPSCrop $1.ps $1.pdf
-rm PSTricksPS.aux PSTricksPS.dvi PSTricksPS.log Fich.tex
-
